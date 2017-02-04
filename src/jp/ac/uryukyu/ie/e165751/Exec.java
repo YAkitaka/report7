f534a1453b90bce3bbc1ec1ffbd7158765b8c3db
Akitaka Yamashita <e165751@ie.uryukyu.ac.jp>
2017-02-05 05:35:25 +0900

    report7

--- src/jp/ac/uryukyu/ie/e165751/Exec.java
+++ src/jp/ac/uryukyu/ie/e165751/Exec.java
@@ -5,11 +5,20 @@ package jp.ac.uryukyu.ie.e165751;
  */
 public class Exec {
     public static void main(String[] args){
+
         CookingRoom room=new CookingRoom(//調理室オブジェクト作成
                 Thread t1=new Thread(new Cook(room)));
                 Thread t2=new Thread(new Cook(room));
                 Thread t3=new Thread(new Cook(room));
+                Thread t4=new Thread(new Waiter(room));
+                Thread t5=new Thread(new Waiter(room));
+                Thread t6=new Thread(new Waiter(room));
+        //CookとWaiterオブジェクトはどれもroomを使って初期化
 
+        //
+        t1.start();t2.start();t3.start();
+        t4.start();t5.start();t6.start();
+        //6つのスレッドを起動する
 
     }
 }
