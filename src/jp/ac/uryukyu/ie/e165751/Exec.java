bb190102d816530e2df896b56a87e5fdbd71b9fd
Akitaka Yamashita <e165751@ie.uryukyu.ac.jp>
2017-02-05 03:42:52 +0900

    report7

--- src/jp/ac/uryukyu/ie/e165751/Exec.java
+++ src/jp/ac/uryukyu/ie/e165751/Exec.java
@@ -4,4 +4,9 @@ package jp.ac.uryukyu.ie.e165751;
  * Created by e165751 on 2017/02/05.
  */
 public class Exec {
+    public static void main(String[] args){
+        CookingRoom room=new CookingRoom(//調理室オブジェクト作成
+                
+
+    }
 }
