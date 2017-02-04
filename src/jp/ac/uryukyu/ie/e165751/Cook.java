bccfe7b8f97a7bc604683ed94f3469642c33d42b
Akitaka Yamashita <e165751@ie.uryukyu.ac.jp>
2017-02-05 03:37:46 +0900

    report7

--- src/jp/ac/uryukyu/ie/e165751/Cook.java
+++ src/jp/ac/uryukyu/ie/e165751/Cook.java
@@ -3,5 +3,12 @@ package jp.ac.uryukyu.ie.e165751;
 /**
  * Created by e165751 on 2017/02/05.
  */
-public class Cook {
+class Cook implements Runnable{
+    CookingRoom room;//調理室の参照
+    public Cook(CookingRoom room){//コンストラクタ。参照を受け取る
+        this.room=room;
+            }
+    public void run(){
+        room.cook();
+    }
 }
