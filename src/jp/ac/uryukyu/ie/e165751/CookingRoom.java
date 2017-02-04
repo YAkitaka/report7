d4e1f385e5cd4a80c098be3c4812786b5f754200
Akitaka Yamashita <e165751@ie.uryukyu.ac.jp>
2017-02-05 05:14:16 +0900

    report7

--- src/jp/ac/uryukyu/ie/e165751/CookingRoom.java
+++ src/jp/ac/uryukyu/ie/e165751/CookingRoom.java
@@ -3,5 +3,29 @@ package jp.ac.uryukyu.ie.e165751;
 /**
  * Created by e165751 on 2017/02/05.
  */
-public class CookingRoom {
+class CookingRoom {
+    boolean flag;
+    int dish=0;
+    //
+    synchronized void cook(){
+        while(flag){
+            try{
+                wait();
+            }catch(InterruptedException e){
+            }
+        }
+    dish++;
+        flag=true;
+        notifyAll();
+
+    }
+    synchronized void spread(){
+        while(!flag){
+            try{
+                wait();
+            }catch(InterruptedException e){
+
+            }
+        }
+    }
 }
