c4502fbd147396e121121d116b899e4e58f59c6a
Akitaka Yamashita <e165751@ie.uryukyu.ac.jp>
2017-02-05 05:19:44 +0900

    report7

--- src/jp/ac/uryukyu/ie/e165751/Waiter.java
+++ src/jp/ac/uryukyu/ie/e165751/Waiter.java
@@ -3,6 +3,13 @@ package jp.ac.uryukyu.ie.e165751;
 /**
  * Created by e165751 on 2017/02/05.
  */
-public class Waiter {
-    
+class Waiter implements Runnable {
+    CookingRoom room;//調理室の参照
+    public Waiter(CookingRoom room){//コンストラクタ。参照を受け取る
+        this.room=room;
+
+    }
+    public void runn(){
+        room.spread();//調理室の配膳メソッド
+    }
 }
